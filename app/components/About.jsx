03c98c3e629a5f98c6a8cc05a132026d9f832145
anilyadav0489 <anilyadav0489@gmail.com>
2018-01-19 13:10:51 -0600

    Updated About and Example sections

--- app/components/About.jsx
+++ app/components/About.jsx
@@ -2,7 +2,10 @@ var React = require('react');
 
 var About = (props) => {
   return (
+    <div>
       <h2>About component</h2>
+      <p>This is About Section of React Weather application</p>
+    </div>
   );
 }
 
