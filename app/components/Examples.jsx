dfbeab64f2c75dbc6df7d0ff64e258bad0a9b658
anilyadav0489 <anilyadav0489@gmail.com>
2018-01-22 16:12:26 -0600

    Updated all files

--- app/components/Examples.jsx
+++ app/components/Examples.jsx
@@ -4,16 +4,16 @@ var {Link} = require('react-router');
 var Examples = (props) => {
   return (
     <div>
-      <h1 className="text-center">Examples</h1>
-      <p>Here are a few example location to try out:</p>
-        <ol>
-          <li>
-            <Link to='/?location=Memphis'>Memphis, TN</Link>
-          </li>
-          <li>
-            <Link to='/?location=Rio'>Rio, Brazil</Link>
-          </li>
-        </ol>
+      <h1 className="text-center page-title">Examples</h1>
+      <p>Here are a few example locations to try out:</p>
+      <ol>
+        <li>
+          <Link to='/?location=Philadelphia'>Philadelphia, PA</Link>
+        </li>
+        <li>
+          <Link to='/?location=Rio'>Rio, Brazil</Link>
+        </li>
+      </ol>
     </div>
   )
 };
--- app/components/Nav.jsx
+++ app/components/Nav.jsx
@@ -2,35 +2,35 @@ var React = require('react');
 var {Link, IndexLink} = require('react-router');
 
 var Nav = React.createClass({
-  onSearch: function(e){
-    e.preventDefault();
-    alert('This functionality has not been implemented yet!!');
+  onSearch: function (e) {
+      e.preventDefault();
+      alert('Not yet wired up!');
   },
-  render: function(){
+  render: function () {
     return (
       <div className="top-bar">
-        <div className = "top-bar-left">
+        <div className="top-bar-left">
           <ul className="menu">
-            <li className="menu-text">Weather App</li>
-              <li>
-                <IndexLink to="/" activeClassName="active" activeStyle={{fontWeight: 'bold'}}>Get Weather</IndexLink>
-              </li>
-              <li>
-                <Link to="/about" activeClassName="active"  activeStyle={{fontWeight: 'bold'}}>About</Link>
-              </li>
-              <li>
-                <Link to="/examples" activeClassName="active" activeStyle={{fontWeight: 'bold'}}>Examples</Link>
-              </li>
+            <li className="menu-text">React Weather App</li>
+            <li>
+              <IndexLink to="/" activeClassName="active" activeStyle={{fontWeight: 'bold'}}>Get Weather</IndexLink>
+            </li>
+            <li>
+              <Link to="/about" activeClassName="active"  activeStyle={{fontWeight: 'bold'}}>About</Link>
+            </li>
+            <li>
+              <Link to="/examples" activeClassName="active" activeStyle={{fontWeight: 'bold'}}>Examples</Link>
+            </li>
           </ul>
         </div>
-        <div className = "top-bar-right">
+        <div className="top-bar-right">
           <form onSubmit={this.onSearch}>
             <ul className="menu">
               <li>
-                <input type="search" placeholder = "Search by location"/>
+                <input type="search" placeholder="Search weather by city"/>
               </li>
               <li>
-                <input type="submit" className = "button" value="Get Weather"/>
+                <input type="submit" className="button" value="Get Weather"/>
               </li>
             </ul>
           </form>
